cf84f68097788228c9b25f4ffcca6f1ba03acfda
Noam <gal.n@husky.neu.edu>
2022-06-28 17:02:38 -0400

    some changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
-import { ReactComponent as DarkLogo } from '../src/assets/logo-dark.svg';
-import './App.css';
-import Layout from './components/Layout';
-import ListPatients from './components/ListPatients';
-import ViewPatient from './components/ViewPatient';
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import ListPatients from "./components/ListPatients";
+import ViewPatient from "./components/ViewPatient";
 
 export function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Layout>
-        <DarkLogo className="logo" />
         <Routes>
           <Route path="/" element={<Navigate to="/patients" />} />
           <Route path="/patients/:id" element={<ViewPatient />} />
